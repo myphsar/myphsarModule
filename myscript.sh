63f429d1eb3c39a48417fd3432d9cb4b87e5bc4d
sak <sak@myphsar.com>
2025-10-01 12:02:13 +0700

    INIT MYPHSAR REPO

---
+++ myscript.sh
@@ -0,0 +1,32 @@
+# Navigate to your target folder
+cd /Users/sak/StudioProjects/mps/build/host/outputs/repo/com/myphsar/myphsar_module/flutter_release/1.0.1
+
+# Sign all .jar files
+for file in *.jar; do
+    gpg -ab "$file"
+done
+
+# Sign all .pom files
+for file in *.pom; do
+    gpg -ab "$file"
+done
+# Sign all .jar files
+for file in *.md5; do
+    gpg -ab "$file"
+done
+# Sign all .jar files
+for file in *.sha1; do
+    gpg -ab "$file"
+done
+ # Sign all .jar files
+ for file in *.sha256; do
+     gpg -ab "$file"
+ done
+ # Sign all .jar files
+ for file in *.sha512; do
+     gpg -ab "$file"
+ done
+ # Sign all .jar files
+ for file in *.module; do
+     gpg -ab "$file"
+ done
